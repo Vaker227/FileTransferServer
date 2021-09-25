5bc0de2d06d36b43a25562a41966a1d6306c518b
LinhDo <hoanglinhzed@gmail.com>
2021-09-26 01:04:49 +0700

    fix heroku index

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 module.exports = (app) => {
+  app.get("/", (req, res) => {
+    res.send("home");
+  });
   app.route("/get-public-ip").get((req, res) => {
     const ip = (req.connection && req.connection.remoteAddress) || "";
     const port = (req.connection && req.connection.remotePort) || "";
